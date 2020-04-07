8826bd32dc3cd2b0dd2c09d7eefb7c02e0c3d5b4
SergeyPopov1115 <highkick11153@gmail.com>
2020-04-07 20:55:02 +0300

    crashing fix

--- app/src/main/java/org/webview/MainActivity.java
+++ app/src/main/java/org/webview/MainActivity.java
@@ -2,6 +2,7 @@ package org.webview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +15,8 @@ import android.webkit.WebViewClient;
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
+    Boolean exit = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,14 +38,29 @@ public class MainActivity extends AppCompatActivity {
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 try {
 
-                    Log.i(":", url);
+                    exit = true;
 
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
                 return false;
             }
+
         });
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (!exit) {
+            finish();
+        } else {
+            finish();
+            Intent intent = getIntent();
+            overridePendingTransition(0, 0);
+            startActivity(intent);
+            overridePendingTransition(0, 0);
+            exit = false;
+        }
     }
 }
