96091be2225b41013b9c92f03dbb45a80c7db8a0
SergeyPopov1115 <highkick11153@gmail.com>
2020-04-07 20:22:50 +0300

    subwebClientView

--- app/src/main/java/org/webview/MainActivity.java
+++ app/src/main/java/org/webview/MainActivity.java
@@ -6,21 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
-    private String url;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         webView = findViewById(R.id.webView);
-
-        Intent extra = getIntent();
-        url = extra.getStringExtra("URL");
 
         WebSettings webSettings = webView.getSettings();
 
@@ -30,7 +28,21 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setAppCacheEnabled(true);
         webSettings.setDomStorageEnabled(true);
         webSettings.setUseWideViewPort(true);
-        webView.loadUrl(url);
+        webView.loadUrl("http://liteoffersapps-eu.s3.eu-central-1.amazonaws.com/m.html");
+
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                try {
+
+                    Log.i(":", url);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return false;
+            }
+        });
 
     }
 }
