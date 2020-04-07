becb8aeba3b42af3b4f05bd732ced8d19ad91011
SergeyPopov1115 <highkick11153@gmail.com>
2020-04-07 19:31:13 +0300

    splash screen add

---
+++ app/src/main/java/org/webview/SplashActivity.java
@@ -0,0 +1,27 @@
+package org.webview;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashActivity.this,CategoryActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, 2000);
+
+    }
+}
